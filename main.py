a9c90641c6847fda4a726366b96d971e49bb4a3f
rchakkara <42642661+rchakkara@users.noreply.github.com>
2024-03-26 11:50:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+from langchain_openai.chat_models import ChatOpenAI
+
+model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model="gpt-3.5-turbo")
